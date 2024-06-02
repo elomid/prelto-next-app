25d2122d164a3d4770858aac008f411ab29c6971
Omid Elliyoun <omid.elli@gmail.com>
2024-06-01 22:24:36 -0700

    typo

--- src/components/icon/index.jsx
+++ src/components/icon/index.jsx
@@ -4,7 +4,7 @@ import Collections from "@/components/icon/svg/collections.svg";
 import Answers from "@/components/icon/svg/answers.svg";
 import Settings from "@/components/icon/svg/settings.svg";
 import ExternalLink from "@/components/icon/svg/externalLink.svg";
-import CheckFilled from "@/components/icon/svg/CheckFilled.svg";
+import CheckFilled from "@/components/icon/svg/checkFilled.svg";
 
 const SvgWrapper = (SvgComponent) => {
   const MemoizedSvgComponent = React.memo(
