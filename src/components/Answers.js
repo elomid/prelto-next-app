62c69b303d322df673ba6030ce989cbdb7397da4
Omid Elliyoun <omid.elli@gmail.com>
2024-06-02 22:30:03 -0700

    add auth to answer post call

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,44 +1,71 @@
 import { useState } from "react";
+import { parseCookies } from "nookies";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Markdown from "react-markdown";
-import styles from "./Answers.module.css"; // Import the CSS module
+import Markdown from "markdown-to-jsx";
+
+import styles from "./Answers.module.css";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 function Answers({ collectionId }) {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState([]);
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleAskQuestion = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    setMessages((messages) => [
-      ...messages,
+    setMessages((prevMessages) => [
+      ...prevMessages,
       { type: "question", content: question },
     ]);
+
+    setError(null);
+    setLoading(true);
     setQuestion("");
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/collections/${collectionId}/answer`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ query: question }),
+    const { token } = parseCookies();
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/collections/${collectionId}/answer`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ query: question }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong. Please try again later.");
       }
-    );
-    const data = await response.json();
-    setMessages((messages) => [...messages, { type: "answer", content: data }]);
-    setLoading(false);
+      const data = await response.json();
+      setMessages((messages) => [
+        ...messages,
+        { type: "answer", content: data },
+      ]);
+    } catch (error) {
+      console.error("Error:", error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="flex flex-col gap-4 pb-48">
+      {error && (
+        <Alert variant="destructive">
+          <AlertTitle>Unable to retrieve answer</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
       <div className="messages-container flex-1 flex flex-col gap-3 overflow-hidden">
-        {messages.map((message) => (
+        {messages.map((message, index) => (
           <div
-            key={message.id}
+            key={index}
             className={`border rounded-lg p-8 text-sm  ${
               message.type == "question"
                 ? " font-medium bg-gray-300 text-black"
@@ -46,7 +73,7 @@ function Answers({ collectionId }) {
             }`}
           >
             <Markdown className={`whitespace-pre-wrap ${styles.markdown}`}>
-              {message.content.replace(/\n\s*\n/g, "\n")}
+              {message.content}
             </Markdown>
           </div>
         ))}
