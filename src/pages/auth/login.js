51636208cc4faa300e4979c49d83423d94b6a034
Omid Elliyoun <omid.elli@gmail.com>
2024-06-04 16:13:59 -0700

    remove credentials login

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -3,14 +3,11 @@ import { setCookie } from "nookies";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
 import Link from "next/link";
 import { IconGoogleLogo } from "@/components/icon";
 
@@ -55,37 +52,6 @@ const LoginPage = () => {
           </Button>
         </Link>
       </div>
-      <form onSubmit={handleSubmit}>
-        <CardContent>
-          <div className="grid w-full items-center gap-4 pt-6">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-              />
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button type="submit">Login</Button>
-        </CardFooter>
-      </form>
     </Card>
   );
 };
