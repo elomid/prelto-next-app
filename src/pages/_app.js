a22ecbd2a17ef90f833e570bb38a5bc03460efa2
Omid Elliyoun <omid.elli@gmail.com>
2024-06-05 10:08:02 -0700

    add basic mixpanel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import mixpanel from "../lib/mixpanel";
+
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      mixpanel.track("Page Viewed", { url });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <div className={inter.className}>
       <Component {...pageProps} />
