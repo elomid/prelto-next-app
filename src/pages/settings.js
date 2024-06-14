607c54142201697c22d8855538c9161f3ce8146c
Omid Elliyoun <omid.elli@gmail.com>
2024-06-13 17:39:45 -0700

    fix link import

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -7,6 +7,7 @@ import useUser from "@/hooks/useUser";
 import { Button } from "@/components/ui/button";
 import { fetchResponse } from "@/utils/fetchUtils";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 export default function SettingsPage() {
   const { user, isLoading, isError } = useUser();
